192bdff21710b4cb20bdcda065a62b9cf4e7762d
iFuentes96 <ifuentess1996@gmail.com>
2025-03-17 23:32:53 -0300

    actualisacion app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//ignacio fuentes:
+
+let listaDeAmigos = [];
+let amigosAgregados = document.getElementById("amigo");
+let amigosEnLista = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+
+//funcion agregar amigo a lista
+
+function agregarAmigo(){
+    //mensaje alerta al no ingresar nombre
+    if(amigosAgregados.value == ""){
+        alert("ingresa un nombre valido")
+    }
+    //agregar amigo a lista principal
+    listaDeAmigos.push(amigosAgregados.value);
+
+    //mostrar en pantalla amigos agregados
+    amigosEnLista.innerHTML += `<li>${amigosAgregados.value}</li>`;
+
+    limpiarCaja();
+    
+}
+
+//limpiar caja nombres
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = "";
+}
+
+//sortear amigo
+
+function sortearAmigo(){
+    let numeroRandom = Math.floor(Math.random() * listaDeAmigos.length);
+    let amigoSecreto = listaDeAmigos[numeroRandom];
+    resultado.innerHTML = `<li>tu amigo secreto es: ${amigoSecreto}</li>`
+
+}
